f7a526b664281350ba6d8a5faa5838639818ec33
Antonio Teran <teran@mit.edu>
2018-03-07 18:07:09 -0500

    i/o with external files working

--- frameOfReference/dataParser.py
+++ frameOfReference/dataParser.py
@@ -18,13 +18,14 @@ Usage:		$ ./dataParser.py pathAndNameOfDataFile [separatorCharacter]
 import numpy as np
 import sys, os
 
+status = True
 # Check for included arguments:
-if len(sys.argv) == 3:
+if len(sys.argv) == 4:
     separatorChar = ',' # set default value
     quatColumn = int(sys.argv[2])
     posColumn = int(sys.argv[3])
     datafile = open(sys.argv[1], 'r') # just read the file; no modifications to original
-elif len(sys.argv) == 4:
+elif len(sys.argv) == 5:
     separatorChar = str(sys.argv[4])  # set user defined value
     quatColumn = int(sys.argv[2])     # starting orientation column
     posColumn = int(sys.argv[3])      # starting position column
@@ -32,12 +33,29 @@ elif len(sys.argv) == 4:
 else:
     print('\n\n*********************')
     print('Stopping! Provided ' + str(len(sys.argv)) + ' arguments, while 2 or 3 are needed.')
-    print('Usage: $ ./' str(sys.argv[0]) + 'pathAndNameOfDataFile [separatorCharacter]')
+    print('Usage: $ ./' + str(sys.argv[0]) + 'pathAndNameOfDataFile [separatorCharacter]')
     print('Try again\n\n')
+    status = False
 
-# Iterate over each datafile line, and write out augmented data file:
-for line in datafile:
-    print(line, end='')
+if status:
+    data = []
+    # Iterate over each datafile line, and write out augmented data file:
+    for line in datafile:
+        if line[0] != '#':
+            tempData = line.split(separatorChar)
+            print(tempData)
+            print(tempData[0])
+            print(tempData[1])
+            print(tempData[2])
+            print(tempData[3])
+            print(tempData[4])
+            print(str(float(tempData[2].split('\t')[1])))
+            print(str(float(tempData[2])))
+            print(tempData[2].split('\t'))
+#            print(str(int(tempData[2])))
+            print('------')
+            
+            
 
 
 
@@ -46,4 +64,3 @@ for line in datafile:
 
 
 # end
-return 0
